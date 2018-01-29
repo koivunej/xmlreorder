bf91ca590235823f1ec48faf34f9a1b18e1cc829
Joonas Koivunen <joonas.koivunen@gmail.com>
2018-01-29 13:34:04 +0200

    expand xpath support ... without particular reason

--- src/xpath.rs
+++ src/xpath.rs
@@ -17,7 +17,7 @@ enum Operator {
 }
 
 impl Operator {
-    fn is_match(&self, snapshot: &TrackerSnapshot) -> bool {
+    fn is_match(&mut self, snapshot: &TrackerSnapshot) -> bool {
         match *self {
             Operator::Descendant(ref name) => {
                 snapshot.path.len() > 0 && snapshot.path[0] == name.as_bytes()
@@ -28,15 +28,22 @@ impl Operator {
             Operator::Wildcard => {
                 !snapshot.path.is_empty()
             },
-            Operator::Position(ref inner, one_based_index) => {
-                // this cannot handle (/a/*)[2]
-                let counts = snapshot.counts;
-                inner.is_match(snapshot) && counts[counts.len() - 1] + 1 == one_based_index
+            Operator::Position(ref mut inner, ref mut one_based_index) => {
+                if one_based_index == &0 {
+                    return false;
+                }
+
+                if inner.is_match(snapshot) {
+                    *one_based_index -= 1;
+                    return one_based_index == &0;
+                }
+
+                return false;
             },
-            Operator::List(ref inners) => {
+            Operator::List(ref mut inners) => {
                 let mut snapshot = *snapshot;
 
-                for inner in inners {
+                for inner in inners.iter_mut() {
                     let m = inner.is_match(&snapshot);
                     if !m {
                         return false;
@@ -60,7 +67,7 @@ impl Selector {
         self.tracker.on_event(span, event);
     }
 
-    pub fn is_match(&self) -> bool {
+    pub fn is_match(&mut self) -> bool {
         self.operators.is_match(&self.tracker.snapshot())
     }
 
@@ -70,7 +77,10 @@ impl Selector {
 
         let mut operators = Vec::new();
 
+        let mut token_buffer = Vec::new();
+
         loop {
+
             let next = buffer.take().or_else(|| tokens.next());
             match next {
                 Some(Token::Slash) => {
@@ -94,6 +104,66 @@ impl Selector {
                 Some(Token::Name(_)) => {
                     panic!("Invalid top level name: {}", s);
                 }
+                Some(token @ Token::OpenParen) => {
+                    token_buffer.push((token, operators.len()));
+                },
+                Some(Token::CloseParen) => {
+                    if let Some((Token::OpenParen, count)) = token_buffer.pop() {
+                        let mut sublist = operators.split_off(count);
+
+                        let inner;
+                        if sublist.len() > 1 {
+                            inner = Operator::List(sublist);
+                        } else if sublist.len() > 0 {
+                            inner = sublist.pop().unwrap();
+                        } else {
+                            panic!("Empty parens: {}", s);
+                        }
+
+                        operators.push(inner);
+                    } else {
+                        panic!("CloseParen without matching OpenParen: {}", s);
+                    }
+                },
+                Some(token @ Token::OpenBracket) => {
+                    if operators.is_empty() {
+                        panic!("OpenBracket without preceding operator: {}", s);
+                    }
+
+                    token_buffer.push((token, operators.len()));
+                },
+                Some(token @ Token::Number(_)) => {
+                    match token_buffer.last() {
+                        Some(&(Token::OpenBracket, _)) => { /* expected */ },
+                        _ => panic!("Numbers outside [...] not supported: {}", s),
+                    }
+
+                    token_buffer.push((token, operators.len()));
+                }
+                Some(Token::CloseBracket) => {
+                    let number = token_buffer.pop();
+                    let open_bracket = token_buffer.pop();
+
+                    match (open_bracket, number) {
+                        (Some((Token::OpenBracket, first_count)), Some((Token::Number(n), last_count))) => {
+                            if first_count != last_count {
+                                panic!("Anything other than number inside brackets is unsupported: {}", s);
+                            }
+
+                            assert_eq!(first_count, operators.len());
+
+                            if n < 1 {
+                                panic!("Position should be 1..: {}", s);
+                            }
+
+                            let preceding = operators.pop().unwrap();
+                            operators.push(Operator::Position(Box::new(preceding), n as usize));
+                        }
+                        _ => {
+                            panic!("Invalid tokens preceding closebracket: {}", s);
+                        }
+                    }
+                }
                 Some(other) => panic!("Unsupported: {}", s),
                 None => break,
             }
@@ -116,7 +186,8 @@ impl Selector {
 fn parse_simple() {
     use super::Positioning;
     use quick_xml::reader::Reader;
-    let mut s = Selector::parse("/foo/bar/car");
+    let mut s1 = Selector::parse("/foo/bar/car");
+    let mut s2 = Selector::parse("(//car)[3]");
 
     let input =
         r#"<foo>
@@ -132,7 +203,7 @@ fn parse_simple() {
              </d>
              <bar>
                 <c />
-                <car>foo</car> <!-- third -->
+                <car>foobar</car> <!-- third -->
                 <car>bar</car> <!-- fourth -->
             </bar>
           </foo>"#;
@@ -140,19 +211,30 @@ fn parse_simple() {
     let mut reader = Reader::from_str(input);
     let mut buffer = Vec::new();
 
-    let mut matches = 0;
+    let mut s1_matches = 0;
+    let mut s2_matches = 0;
+    let mut s2_text = None;
 
     loop {
         let evt = reader.read_event(&mut buffer).unwrap();
-        s.update_on_event(reader.span_for(&evt), &evt);
+        s1.update_on_event(reader.span_for(&evt), &evt);
+        s2.update_on_event(reader.span_for(&evt), &evt);
         match evt {
-            Event::Start(ref e) | Event::Empty(ref e) => if s.is_match() { matches += 1; },
+            Event::Start(ref e) | Event::Empty(ref e) => {
+                if s1.is_match() { s1_matches += 1; }
+                if s2.is_match() { s2_matches += 1; }
+            },
+            Event::Text(ref e) if s2_matches == 1 && !e.is_empty() && s2_text.is_none() => {
+                s2_text = Some(e.unescaped().unwrap().into_owned());
+            }
             Event::Eof => break,
             _ => {},
         }
     }
 
-    assert_eq!(matches, 4);
+    assert_eq!(s1_matches, 4);
+    assert_eq!(s2_matches, 1);
+    assert_eq!(s2_text, Some(b"foobar".to_vec()));
 }
 
 #[derive(Debug)]
@@ -160,6 +242,7 @@ enum Token {
     Slash,
     DoubleSlash,
     Name(String),
+    Number(isize),
     Dot,
     Star,
     OpenBracket,
@@ -215,6 +298,26 @@ impl<'a> Iterator for Tokenizer<'a> {
             Some(']') => Some(Token::CloseBracket),
             Some('.') => Some(Token::Dot),
             Some('*') => Some(Token::Star),
+            Some(ch @ '0'...'9') => {
+                let mut s = String::new();
+
+                s.push(ch);
+
+                loop {
+                    match self.chars.next() {
+                        Some(ch @ '0'...'9') => s.push(ch),
+                        Some(other) => {
+                            self.buffer = Some(other);
+                            return Some(Token::Number(s.parse::<isize>().expect("Failed to parse digits")));
+                        },
+                        None => {
+                            self.eoffed = true;
+                            return Some(Token::Number(s.parse::<isize>().expect("Failed to parse digits")));
+                        }
+                    }
+                }
+
+            },
             Some(other) => {
                 let mut s = String::new();
 
